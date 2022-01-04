1b4d7f9c18793b7982d626921eb186542d6925b7
rifqiagniamubarok <rifqiagniamubarok@gmail.com>
2022-01-04 13:28:18 +0700

    menambahkan hide dam unhide psw

---
+++ src/pages/userPages/DashboardPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserDashboardNavbarComponent from "../../views/UserDashboardNavbarComponent";
+
+function DashboardPage() {
+  return (
+    <div>
+      <UserDashboardNavbarComponent />
+    </div>
+  );
+}
+
+export default DashboardPage;
