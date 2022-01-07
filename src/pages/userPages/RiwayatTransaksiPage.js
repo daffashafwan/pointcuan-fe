398f3c622bd93ab68d07cc0a4f1f7007b8d4001a
rifqiagniamubarok <rifqiagniamubarok@gmail.com>
2022-01-07 14:55:54 +0700

    menambahkan riwayat transaksi

--- src/pages/userPages/RiwayatTransaksiPage.js
+++ src/pages/userPages/RiwayatTransaksiPage.js
@@ -2,18 +2,57 @@ import React from "react";
 import { useState } from "react/cjs/react.development";
 import UserDashboardNavbarComponent from "../../views/UserDashboardNavbarComponent";
 import UserFooterComponent from "../../views/UserFooterComponent";
+import { AiFillEye, AiOutlineClose } from "react-icons/ai";
+import TableRiwayatTransaksi from "../../components/TableRiwayatTransaksi";
 
 function RiwayatTransaksiPage() {
-  const [statiusTransaksi, setStatusTransaksi] = useState(0);
+  const [statusTransaksi, setStatusTransaksi] = useState(0);
   return (
     <div>
       <UserDashboardNavbarComponent />
-      <div className="container">
+      <div className="container mx-auto pt-20">
         <div>
-          <h1>Daftar Transaksi</h1>
-          <span>Proses</span>
-          <span>Ditolak</span>
-          <span>Diterima</span>
+          <h1 className="md:mx-10 text-center md:text-left font-bold text-3xl mt-2">
+            Daftar Transaksi
+          </h1>
+          <div className="mt-7 mx-2 md:mx-10 text-center md:text-left">
+            <span
+              className={
+                "border border-gray-400 border-2 rounded-md md:rounded-xl px-4 md:px-6 py-2 mx-2 text-xs md:text-lg font-semibold cursor-pointer " +
+                (statusTransaksi === 0
+                  ? "text-white bg-orange-500 hover:bg-orange-400 text-white "
+                  : " text-black bg-white hover:bg-gray-200 text-black")
+              }
+              onClick={() => setStatusTransaksi(0)}
+            >
+              Proses
+            </span>
+            <span
+              className={
+                "border border-gray-400 border-2 rounded-md md:rounded-xl px-4 md:px-6 py-2 mx-2 text-xs md:text-lg font-semibold cursor-pointer " +
+                (statusTransaksi === 1
+                  ? "text-white bg-orange-500 hover:bg-orange-400 text-white "
+                  : " text-black bg-white hover:bg-gray-200 text-black")
+              }
+              onClick={() => setStatusTransaksi(1)}
+            >
+              Ditolak
+            </span>
+            <span
+              className={
+                "border border-gray-400 border-2 rounded-md md:rounded-xl px-4 md:px-6 py-2 mx-2 text-xs md:text-lg font-semibold cursor-pointer " +
+                (statusTransaksi === 2
+                  ? "text-white bg-orange-500 hover:bg-orange-400 text-white "
+                  : " text-black bg-white hover:bg-gray-200 text-black")
+              }
+              onClick={() => setStatusTransaksi(2)}
+            >
+              Diterima
+            </span>
+          </div>
+        </div>
+        <div>
+          <TableRiwayatTransaksi statusTransaksi={statusTransaksi} />
         </div>
       </div>
       <UserFooterComponent />
